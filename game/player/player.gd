0a57aa9d3f04dd7b1e12731dda561b16873f8dc8
Ignacio Aguirrezabal <zirion0@gmail.com>
2017-08-05 16:04:06 -0300

    Add sprite animation to player.

--- game/player/player.gd
+++ game/player/player.gd
@@ -10,9 +10,11 @@ func _fixed_process(delta):
 	
 	velocity.x = 0
 	velocity.y = 0
+	get_node("AnimatedSprite/AnimationPlayer").play("idle")
 	
 	if (Input.is_action_pressed("walk_left")):
 		velocity.x = -player_speed
+		get_node("AnimatedSprite/AnimationPlayer").play("left")
 	elif (Input.is_action_pressed("walk_right")):
 		velocity.x = player_speed
 		
@@ -20,6 +22,10 @@ func _fixed_process(delta):
 		velocity.y = -player_speed
 	elif (Input.is_action_pressed("walk_down")):
 		velocity.y = player_speed
+		get_node("AnimatedSprite/AnimationPlayer").play("down")
+		
+	if (Input.is_action_pressed("walk_down") and Input.is_action_pressed("walk_right")):
+		get_node("AnimatedSprite/AnimationPlayer").play("down_right")
 
 	var motion = velocity * delta
 	motion = move(motion)
